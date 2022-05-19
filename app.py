e47e2e6c6d5fe2505b2e1875a553aba7b87b9a86
hysts <hysts@users.noreply.huggingface.co>
2022-05-20 05:20:14 +0900

    Update

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ import os
 import pickle
 import sys
 
-sys.path.insert(0, 'projected_gan')
-
 import gradio as gr
 import numpy as np
 import torch
 import torch.nn as nn
 from huggingface_hub import hf_hub_download
+
+sys.path.insert(0, 'projected_gan')
 
 ORIGINAL_REPO_URL = 'https://github.com/autonomousvision/projected_gan'
 TITLE = 'autonomousvision/projected_gan'
@@ -99,8 +99,6 @@ def load_model(model_name: str, device: torch.device) -> nn.Module:
 
 
 def main():
-    gr.close_all()
-
     args = parse_args()
     device = torch.device(args.device)
 
