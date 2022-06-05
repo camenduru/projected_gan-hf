afe6a1b40b8a16598b5700b1527d9d6885a74232
hysts <hysts@users.noreply.huggingface.co>
2022-06-06 04:54:14 +0900

    Update

--- app.py
+++ app.py
@@ -3,144 +3,101 @@
 from __future__ import annotations
 
 import argparse
-import functools
-import os
-import pickle
-import sys
 
 import gradio as gr
 import numpy as np
-import torch
-import torch.nn as nn
-from huggingface_hub import hf_hub_download
 
-sys.path.insert(0, 'projected_gan')
+from model import Model
 
-TITLE = 'autonomousvision/projected_gan'
-DESCRIPTION = '''This is an unofficial demo for https://github.com/autonomousvision/projected_gan.
+TITLE = '# autonomousvision/projected_gan'
+DESCRIPTION = '''This is an unofficial demo for [https://github.com/autonomousvision/projected_gan](https://github.com/autonomousvision/projected_gan).
 
 Expected execution time on Hugging Face Spaces: 1s
 '''
-SAMPLE_IMAGE_DIR = 'https://huggingface.co/spaces/hysts/projected_gan/resolve/main/samples'
-ARTICLE = f'''## Generated images
-- truncation: 0.7
-- size: 256x256
-- seed: 0-99
-### Art painting
-![Art painting samples]({SAMPLE_IMAGE_DIR}/art_painting.jpg)
-### Bedroom
-![Bedroom samples]({SAMPLE_IMAGE_DIR}/bedroom.jpg)
-### Church
-![Church samples]({SAMPLE_IMAGE_DIR}/church.jpg)
-### Cityscapes
-![Cityscapes samples]({SAMPLE_IMAGE_DIR}/cityscapes.jpg)
-### CLEVR
-![CLEVR samples]({SAMPLE_IMAGE_DIR}/clevr.jpg)
-### FFHQ
-![FFHQ samples]({SAMPLE_IMAGE_DIR}/ffhq.jpg)
-### Flowers
-![Flowers samples]({SAMPLE_IMAGE_DIR}/flowers.jpg)
-### Landscape
-![Landscape samples]({SAMPLE_IMAGE_DIR}/landscape.jpg)
-### Pokemon
-![Pokemon samples]({SAMPLE_IMAGE_DIR}/pokemon.jpg)
-
-<center><img src="https://visitor-badge.glitch.me/badge?page_id=hysts.projected_gan" alt="visitor badge"/></center>
-'''
-
-TOKEN = os.environ['TOKEN']
+FOOTER = '<img id="visitor-badge" alt="visitor badge" src="https://visitor-badge.glitch.me/badge?page_id=hysts.projected_gan" />'
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('--device', type=str, default='cpu')
     parser.add_argument('--theme', type=str)
-    parser.add_argument('--live', action='store_true')
     parser.add_argument('--share', action='store_true')
     parser.add_argument('--port', type=int)
     parser.add_argument('--disable-queue',
                         dest='enable_queue',
                         action='store_false')
-    parser.add_argument('--allow-flagging', type=str, default='never')
     return parser.parse_args()
 
 
-def generate_z(z_dim: int, seed: int, device: torch.device) -> torch.Tensor:
-    return torch.from_numpy(
-        np.random.RandomState(seed).randn(1,
-                                          z_dim).astype(np.float32)).to(device)
+def get_sample_image_url(name: str) -> str:
+    sample_image_dir = 'https://huggingface.co/spaces/hysts/projected_gan/resolve/main/samples'
+    return f'{sample_image_dir}/{name}.jpg'
 
 
-@torch.inference_mode()
-def generate_image(model_name: str, seed: int, truncation_psi: float,
-                   model_dict: dict[str, nn.Module],
-                   device: torch.device) -> np.ndarray:
-    model = model_dict[model_name]
-    seed = int(np.clip(seed, 0, np.iinfo(np.uint32).max))
-
-    z = generate_z(model.z_dim, seed, device)
-    label = torch.zeros([1, model.c_dim], device=device)
-
-    out = model(z, label, truncation_psi=truncation_psi)
-    out = (out.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-    return out[0].cpu().numpy()
-
-
-def load_model(model_name: str, device: torch.device) -> nn.Module:
-    path = hf_hub_download('hysts/projected_gan',
-                           f'models/{model_name}.pkl',
-                           use_auth_token=TOKEN)
-    with open(path, 'rb') as f:
-        model = pickle.load(f)['G_ema']
-    model.eval()
-    model.to(device)
-    with torch.inference_mode():
-        z = torch.zeros((1, model.z_dim)).to(device)
-        label = torch.zeros([1, model.c_dim], device=device)
-        model(z, label)
-    return model
+def get_sample_image_markdown(name: str) -> str:
+    url = get_sample_image_url(name)
+    return f'''
+    - size: 256x256
+    - seed: 0-99
+    - truncation: 0.7
+    ![sample images]({url})'''
 
 
 def main():
     args = parse_args()
-    device = torch.device(args.device)
+    model = Model(args.device)
 
-    model_names = [
-        'art_painting',
-        'church',
-        'bedroom',
-        'cityscapes',
-        'clevr',
-        'ffhq',
-        'flowers',
-        'landscape',
-        'pokemon',
-    ]
+    with gr.Blocks(theme=args.theme, css='style.css') as demo:
+        gr.Markdown(TITLE)
+        gr.Markdown(DESCRIPTION)
 
-    model_dict = {name: load_model(name, device) for name in model_names}
+        with gr.Tabs():
+            with gr.TabItem('App'):
+                with gr.Row():
+                    with gr.Column():
+                        with gr.Group():
+                            model_name = gr.Dropdown(
+                                model.MODEL_NAMES,
+                                value=model.MODEL_NAMES[8],
+                                label='Model')
+                            seed = gr.Slider(0,
+                                             np.iinfo(np.uint32).max,
+                                             step=1,
+                                             value=0,
+                                             label='Seed')
+                            psi = gr.Slider(0,
+                                            2,
+                                            step=0.05,
+                                            value=0.7,
+                                            label='Truncation psi')
+                            run_button = gr.Button('Run')
+                    with gr.Column():
+                        result = gr.Image(label='Result', elem_id='result')
 
-    func = functools.partial(generate_image,
-                             model_dict=model_dict,
-                             device=device)
-    func = functools.update_wrapper(func, generate_image)
+            with gr.TabItem('Sample Images'):
+                with gr.Row():
+                    model_name2 = gr.Dropdown(model.MODEL_NAMES,
+                                              value=model.MODEL_NAMES[0],
+                                              label='Model')
+                with gr.Row():
+                    text = get_sample_image_markdown(model_name2.value)
+                    sample_images = gr.Markdown(text)
 
-    gr.Interface(
-        func,
-        [
-            gr.inputs.Radio(
-                model_names, type='value', default='pokemon', label='Model'),
-            gr.inputs.Number(default=0, label='Seed'),
-            gr.inputs.Slider(
-                0, 2, step=0.05, default=0.7, label='Truncation psi'),
-        ],
-        gr.outputs.Image(type='numpy', label='Output'),
-        title=TITLE,
-        description=DESCRIPTION,
-        article=ARTICLE,
-        theme=args.theme,
-        allow_flagging=args.allow_flagging,
-        live=args.live,
-    ).launch(
+        gr.Markdown(FOOTER)
+
+        model_name.change(fn=model.set_model, inputs=model_name, outputs=None)
+        run_button.click(fn=model.set_model_and_generate_image,
+                         inputs=[
+                             model_name,
+                             seed,
+                             psi,
+                         ],
+                         outputs=result)
+        model_name2.change(fn=get_sample_image_markdown,
+                           inputs=model_name2,
+                           outputs=sample_images)
+
+    demo.launch(
         enable_queue=args.enable_queue,
         server_port=args.port,
         share=args.share,
---
+++ model.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+import os
+import pathlib
+import pickle
+import sys
+
+import numpy as np
+import torch
+import torch.nn as nn
+from huggingface_hub import hf_hub_download
+
+current_dir = pathlib.Path(__file__).parent
+submodule_dir = current_dir / 'projected_gan'
+sys.path.insert(0, submodule_dir.as_posix())
+
+HF_TOKEN = os.environ['HF_TOKEN']
+
+
+class Model:
+
+    MODEL_NAMES = [
+        'art_painting',
+        'church',
+        'bedroom',
+        'cityscapes',
+        'clevr',
+        'ffhq',
+        'flowers',
+        'landscape',
+        'pokemon',
+    ]
+
+    def __init__(self, device: str | torch.device):
+        self.device = torch.device(device)
+        self._download_all_models()
+        self.model_name = self.MODEL_NAMES[3]
+        self.model = self._load_model(self.model_name)
+
+    def _load_model(self, model_name: str) -> nn.Module:
+        path = hf_hub_download('hysts/projected_gan',
+                               f'models/{model_name}.pkl',
+                               use_auth_token=HF_TOKEN)
+        with open(path, 'rb') as f:
+            model = pickle.load(f)['G_ema']
+        model.eval()
+        model.to(self.device)
+        return model
+
+    def set_model(self, model_name: str) -> None:
+        if model_name == self.model_name:
+            return
+        self.model_name = model_name
+        self.model = self._load_model(model_name)
+
+    def _download_all_models(self):
+        for name in self.MODEL_NAMES:
+            self._load_model(name)
+
+    def generate_z(self, seed: int) -> torch.Tensor:
+        seed = int(np.clip(seed, 0, np.iinfo(np.uint32).max))
+        z = np.random.RandomState(seed).randn(1, self.model.z_dim)
+        return torch.from_numpy(z).float().to(self.device)
+
+    def postprocess(self, tensor: torch.Tensor) -> np.ndarray:
+        tensor = (tensor.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(
+            torch.uint8)
+        return tensor.cpu().numpy()
+
+    @torch.inference_mode()
+    def generate(self, z: torch.Tensor, label: torch.Tensor,
+                 truncation_psi: float) -> torch.Tensor:
+        return self.model(z, label, truncation_psi=truncation_psi)
+
+    def generate_image(self, seed: int, truncation_psi: float) -> np.ndarray:
+        z = self.generate_z(seed)
+        label = torch.zeros([1, self.model.c_dim], device=self.device)
+
+        out = self.generate(z, label, truncation_psi)
+        out = self.postprocess(out)
+        return out[0]
+
+    def set_model_and_generate_image(self, model_name: str, seed: int,
+                                     truncation_psi: float) -> np.ndarray:
+        self.set_model(model_name)
+        return self.generate_image(seed, truncation_psi)
